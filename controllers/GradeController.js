a2a378c02bea318d8b7d01e73013fa6368a4b60f
Max Calabro <maxccalabro@gmail.com>
2022-11-22 15:15:35 -0500

    check

--- controllers/GradeController.js
+++ controllers/GradeController.js
@@ -19,8 +19,28 @@ const UpdateStudentGrade = async (req, res) => {
   try {
     const classId = req.params.class_id
     const { studentId, letter } = req.body
+    let score = 0
+    switch (letter.toUpperCase()) {
+      case 'A':
+        score = 4
+        break
+      case 'B':
+        score = 3
+        break
+      case 'C':
+        score = 2
+        break
+      case 'D':
+        score = 1
+        break
+      case 'F':
+        score = 0
+        break
+      default:
+        res.send({ msg: 'Not a valid grade' })
+    }
     const updatedGrade = await Grade.update(
-      { letter: letter },
+      { letter: letter.toUpperCase(), score: score },
       {
         where: Sequelize.and({ classId: classId }, { studentId: studentId }),
         returning: true
